41654eb55366d1d6389a8ffb039bc8ba98745a10
David Fermet <david.fermet@optiflows.com>
2015-03-25 11:29:10 +0100

    dsfefe
    sd

--- docs/conf.py
+++ docs/conf.py
@@ -199,7 +199,7 @@ latex_logo = 'images/logo-surycat-brand.png'
 #latex_show_urls = False
 
 # Additional stuff for the LaTeX preamble.
-#latex_preamble = ''
+latex_preamble = '\usepackage[scaled=.90]{helvet}'
 
 # Documents to append as an appendix to all manuals.
 #latex_appendices = []
